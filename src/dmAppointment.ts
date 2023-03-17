e25a9a20b25fc46881bf2f00a3138ec7c5121437
anikaandewa <126886443+anikaandewa@users.noreply.github.com>
2023-03-17 13:27:45 +0100

    version 1

--- src/dmAppointment.ts
+++ src/dmAppointment.ts
@@ -1,4 +1,5 @@
 import { MachineConfig, send, Action, assign } from "xstate";
+import { LEMMAS } from "./wordbank"
 
 function say(text: string): Action<SDSContext, SDSEvent> {
   return send((_context: SDSContext) => ({ type: "SPEAK", value: text }));
@@ -43,6 +44,32 @@ const getEntity = (context: SDSContext, entity: string) => {
   return false;
 };
 
+let listOfUsedWords: string[] = []
+
+const findWord = (context: SDSContext, entity: string) => {
+  // lowercase the utterance and remove tailing "."
+  let u = context.recResult[0].utterance.toLowerCase().replace(/\.$/g, "");
+  listOfUsedWords.push(u)
+  let psi = u.substr(u.length - 1) // the last letter of the word provided by user
+  let siupsiup = LEMMAS.filter((k) => k[0] === psi) // retrieves words from lemmas that start with the letter in psi
+  const randIndex = Math.floor(Math.random() * siupsiup.length); // gives a random index among the retrieved words
+  const word = siupsiup[randIndex];
+  let unusedWords = LEMMAS.filter(word => !listOfUsedWords.includes(word))
+  if (listOfUsedWords.includes(word))
+  {
+    if (!((unusedWords.filter(word => word.startsWith(psi))).length === 0)) {
+    let word1 = unusedWords[Math.floor(Math.random() * unusedWords.length)]
+    listOfUsedWords.push(word1)
+    return word1
+  }
+  return "I cannot think of anything else. You win."
+}
+  else {
+    listOfUsedWords.push(word)
+    return word
+  }
+};
+
 export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
   initial: "idle",
   states: {
@@ -62,10 +89,10 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
       on: {
         RECOGNISED: [
           {
-            target: "info",
-            cond: (context) => !!getEntity(context, "title"),
+            target: "giveword",
+            cond: (context) => !!findWord(context, "title"),
             actions: assign({
-              title: (context) => getEntity(context, "title"),
+              title: (context) => findWord(context, "title"),
             }),
           },
           {
@@ -76,7 +103,7 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
       },
       states: {
         prompt: {
-          entry: say("Let's create a meeting. What is it about?"),
+          entry: say("Hello, welcome to the Word Chain Game. The rules are simple - you give me a word and I respond with a word that starts with the last letter of your word. And then you do the same! The starting word is: lunch"),
           on: { ENDSPEECH: "ask" },
         },
         ask: {
@@ -90,12 +117,79 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
         },
       },
     },
-    info: {
-      entry: send((context) => ({
-        type: "SPEAK",
-        value: `OK, ${context.title}`,
-      })),
-      on: { ENDSPEECH: "init" },
+    giveword: {
+      id: "giveword",
+      initial: "prompt",
+      on: {
+        RECOGNISED: [
+          {
+            target: "giveword2",
+            cond: (context) => !!findWord(context, "title"),
+            actions: assign({
+              title: (context) => findWord(context, "title"),
+            }),
+          },
+          {
+            target: ".nomatch",
+          },
+        ],
+        TIMEOUT: ".prompt",
+      },
+      states: {
+        prompt: {
+          entry: send((context) => ({
+            type: "SPEAK",
+            value: `The word is: ${context.title}`,
+          })),
+          on: { ENDSPEECH: "ask" },
+        },
+        ask: {
+          entry: send("LISTEN"),
+        },
+        nomatch: {
+          entry: say(
+            "Sorry, I don't know what it is. Tell me something I know."
+          ),
+          on: { ENDSPEECH: "ask" },
+        },
+      },
+    },
+    giveword2: {
+      id: "giveword",
+      initial: "prompt",
+      on: {
+        RECOGNISED: [
+          {
+            target: "giveword",
+            cond: (context) => !!findWord(context, "title"),
+            actions: assign({
+              title: (context) => findWord(context, "title"),
+            }),
+          },
+          {
+            target: ".nomatch",
+          },
+        ],
+        TIMEOUT: ".prompt",
+      },
+      states: {
+        prompt: {
+          entry: send((context) => ({
+            type: "SPEAK",
+            value: `The word is: ${context.title}`,
+          })),
+          on: { ENDSPEECH: "ask" },
+        },
+        ask: {
+          entry: send("LISTEN"),
+        },
+        nomatch: {
+          entry: say(
+            "Sorry, I don't know what it is. Tell me something I know."
+          ),
+          on: { ENDSPEECH: "ask" },
+        },
+      },
     },
   },
 };
